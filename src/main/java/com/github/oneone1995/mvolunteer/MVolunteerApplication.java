4d5cc08de9c352fc91d47e0f6495e98332a1094a
oneone1995 <wanglixian1995@live.com>
2017-02-08 21:44:19 +0800

    更改mybatis包扫描方式为java代码配置

--- src/main/java/com/github/oneone1995/mvolunteer/MVolunteerApplication.java
+++ src/main/java/com/github/oneone1995/mvolunteer/MVolunteerApplication.java
@@ -1,14 +1,10 @@
 package com.github.oneone1995.mvolunteer;
 
-import com.github.oneone1995.mvolunteer.util.MyMapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.github.oneone1995.mvolunteer.mapper", markerInterface = MyMapper.class)
 public class MVolunteerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MVolunteerApplication.class, args);
 	}
---
+++ src/main/java/com/github/oneone1995/mvolunteer/config/mybatis/MyBatisMapperScannerConfig.java
@@ -0,0 +1,32 @@
+package com.github.oneone1995.mvolunteer.config.mybatis;
+
+import com.github.oneone1995.mvolunteer.util.MyMapper;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tk.mybatis.spring.mapper.MapperScannerConfigurer;
+
+import java.util.Properties;
+
+/**
+ * Created by wangl on 2017/2/8.
+ */
+@Configuration
+@AutoConfigureAfter(MybatisAutoConfiguration.class)
+public class MyBatisMapperScannerConfig {
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+        mapperScannerConfigurer.setBasePackage("com.github.oneone1995.mvolunteer.mapper");
+        Properties properties = new Properties();
+        // 这里要特别注意，不要把MyMapper放到 basePackage 中，也就是不能同其他Mapper一样被扫描到。
+        properties.setProperty("mappers", MyMapper.class.getName());
+        properties.setProperty("notEmpty", "false");
+        properties.setProperty("IDENTITY", "MYSQL");
+        mapperScannerConfigurer.setProperties(properties);
+        return mapperScannerConfigurer;
+    }
+
+}
