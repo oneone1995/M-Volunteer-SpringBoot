73966291686dc1634b9cf1762493cfb0316e7b07
oneone1995 <wanglixian1995@live.com>
2017-02-11 15:50:05 +0800

    测试权限时的信息类

---
+++ src/main/java/com/github/oneone1995/mvolunteer/model/Msg.java
@@ -0,0 +1,43 @@
+package com.github.oneone1995.mvolunteer.model;
+
+/**
+ * Created by wangl on 2017/2/9.
+ */
+public class Msg {
+    private String title;
+    private String content;
+    private String extraInfo;
+
+    public Msg() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getExtraInfo() {
+        return extraInfo;
+    }
+
+    public void setExtraInfo(String extraInfo) {
+        this.extraInfo = extraInfo;
+    }
+
+    public Msg(String title, String content, String extraInfo) {
+        this.title = title;
+        this.content = content;
+        this.extraInfo = extraInfo;
+    }
+}
