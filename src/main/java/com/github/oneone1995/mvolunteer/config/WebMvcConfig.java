40b4190e30c4f2471b4a8adae3706cef711553d3
oneone1995 <wanglixian1995@live.com>
2017-02-11 15:49:17 +0800

    webMVC相关配置类

---
+++ src/main/java/com/github/oneone1995/mvolunteer/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.github.oneone1995.mvolunteer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by wangl on 2017/2/9.
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
