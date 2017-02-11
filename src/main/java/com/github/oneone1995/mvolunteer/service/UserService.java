e8e52eb93b68f6060026497a170f050be4579d09
oneone1995 <wanglixian1995@live.com>
2017-02-11 15:42:34 +0800

    让UserService接口继承spring-security提供的UserDetailsService

--- src/main/java/com/github/oneone1995/mvolunteer/service/UserService.java
+++ src/main/java/com/github/oneone1995/mvolunteer/service/UserService.java
@@ -2,22 +2,12 @@ package com.github.oneone1995.mvolunteer.service;
 
 
 import com.github.oneone1995.mvolunteer.domain.User;
-import com.github.oneone1995.mvolunteer.mapper.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * Created by wangl on 2017/2/5.
  */
-@Service
-public class UserService {
-    @Autowired
-    private UserMapper userMapper;
 
-    public User findUserById(int id) {
-        return userMapper.selectByPrimaryKey(id);
-    }
-
+public interface UserService extends UserDetailsService{
+    User getUserById(int id);
 }
