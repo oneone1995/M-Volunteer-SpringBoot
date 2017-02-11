a1171b170b9ec7a5533d4123e4948f9c734a3d9c
oneone1995 <wanglixian1995@live.com>
2017-02-11 15:46:27 +0800

    新增spring-security配置文件

---
+++ src/main/java/com/github/oneone1995/mvolunteer/config/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.github.oneone1995.mvolunteer.config.security;
+
+import com.github.oneone1995.mvolunteer.service.impl.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by wangl on 2017/2/10.
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    UserDetailsService customUserService() {
+        return new UserServiceImpl();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserService());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").failureUrl("/login?error").permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
