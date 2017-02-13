0993694528474a2a6626a71ace870806642a5766
oneone1995 <wanglixian1995@live.com>
2017-02-13 14:43:08 +0800

    修改spring-security配置，使基于方法的权限控制生效

--- src/main/java/com/github/oneone1995/mvolunteer/config/security/WebSecurityConfig.java
+++ src/main/java/com/github/oneone1995/mvolunteer/config/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.github.oneone1995.mvolunteer.service.impl.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  * Created by wangl on 2017/2/10.
  */
 @Configuration
+//用于@PreAuthorize的生效,基于方法的权限控制
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     UserDetailsService customUserService() {
