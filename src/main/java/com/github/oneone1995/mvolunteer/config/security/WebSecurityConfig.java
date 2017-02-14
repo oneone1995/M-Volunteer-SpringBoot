5213bd96ea7b3338ecff819162cbf16366db3bfa
oneone1995 <wanglixian1995@live.com>
2017-02-14 21:49:46 +0800

    修改webSecurityConfig继承的父类并重新实现方法

--- src/main/java/com/github/oneone1995/mvolunteer/config/security/WebSecurityConfig.java
+++ src/main/java/com/github/oneone1995/mvolunteer/config/security/WebSecurityConfig.java
@@ -1,13 +1,11 @@
 package com.github.oneone1995.mvolunteer.config.security;
 
-import com.github.oneone1995.mvolunteer.service.impl.UserServiceImpl;
-import org.springframework.context.annotation.Bean;
+import com.github.oneone1995.mvolunteer.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * Created by wangl on 2017/2/10.
@@ -15,21 +13,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @Configuration
 //用于@PreAuthorize的生效,基于方法的权限控制
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    UserDetailsService customUserService() {
-        return new UserServiceImpl();
+public class WebSecurityConfig extends GlobalAuthenticationConfigurerAdapter {
+    private final UserService userService;
+
+    @Autowired
+    public WebSecurityConfig(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserService());
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated();
-
-    }
 }
