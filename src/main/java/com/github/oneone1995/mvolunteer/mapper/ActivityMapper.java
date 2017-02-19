54a97ba3f296579f6276799d4bb49739178f59e3
oneone1995 <wanglixian1995@live.com>
2017-02-19 21:00:11 +0800

    增加mapper接口注释

--- src/main/java/com/github/oneone1995/mvolunteer/mapper/ActivityMapper.java
+++ src/main/java/com/github/oneone1995/mvolunteer/mapper/ActivityMapper.java
@@ -11,7 +11,13 @@ import java.util.List;
  */
 @Mapper
 public interface ActivityMapper {
-
+    /**
+     * 根据活动添加时间排序查询活动列表返回
+     * 并根据参数中的经纬度求出距离并将其包装进HomeActivity中的mapper接口
+     * @param coordLong 经度
+     * @param coordLat  维度
+     * @return  首页展示的活动列表
+     */
     List<HomeActivity> selectAllOderByTime(
             @Param("coordLong") double coordLong, @Param("coordLat") double coordLat);
 }
