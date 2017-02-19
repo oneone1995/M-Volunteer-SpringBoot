3740a9595ede8f9b8696077b32a9551f374eacce
oneone1995 <wanglixian1995@live.com>
2017-02-19 21:33:41 +0800

    新增列表为空的错误码

--- src/main/java/com/github/oneone1995/mvolunteer/config/result/ResultStatus.java
+++ src/main/java/com/github/oneone1995/mvolunteer/config/result/ResultStatus.java
@@ -8,7 +8,8 @@ public enum ResultStatus {
     SUCCESS(200, "成功"),
     USERNAME_OR_PASSWORD_ERROR(-1001, "用户名或密码错误"),
     USER_NOT_FOUND(-1002, "用户不存在"),
-    USER_NOT_LOGIN(-1003, "用户未登录");
+    USER_NOT_LOGIN(-1003, "用户未登录"),
+    ACTIVITY_NOT_FOUNT(-1004, "活动列表为空");
 
     /**
      * 返回码
