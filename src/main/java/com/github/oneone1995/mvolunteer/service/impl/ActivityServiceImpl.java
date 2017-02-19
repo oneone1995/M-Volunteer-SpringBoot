3170c67f6727b009f62d74cb61a594a3a9dc3e2d
oneone1995 <wanglixian1995@live.com>
2017-02-19 21:27:15 +0800

    具体实现分页展示首页的活动列表的接口

--- src/main/java/com/github/oneone1995/mvolunteer/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/github/oneone1995/mvolunteer/service/impl/ActivityServiceImpl.java
@@ -1,10 +1,13 @@
 package com.github.oneone1995.mvolunteer.service.impl;
 
 import com.github.oneone1995.mvolunteer.domain.HomeActivity;
+import com.github.oneone1995.mvolunteer.mapper.ActivityMapper;
 import com.github.oneone1995.mvolunteer.service.ActivityService;
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -12,9 +15,17 @@ import java.util.List;
  */
 @Service
 public class ActivityServiceImpl implements ActivityService {
+    @Resource
+    private ActivityMapper activityMapper;
+
     @Override
     public PageInfo<List<HomeActivity>> getHomeActivityPageInfo(
             Integer page, Integer rows, double coordLong, double coordLat) {
-        return null;
+
+        PageHelper.startPage(page, rows);
+        List<HomeActivity> homeActivityList = activityMapper.selectAllOderByTime(coordLong, coordLat);
+
+        PageInfo homeActivityPageInfo = new PageInfo<>(homeActivityList);
+        return homeActivityPageInfo;
     }
 }
