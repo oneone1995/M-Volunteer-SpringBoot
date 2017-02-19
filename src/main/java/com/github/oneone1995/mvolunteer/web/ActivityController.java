46e8d8679bb0f6eef58741c51b16be78d17bc41d
oneone1995 <wanglixian1995@live.com>
2017-02-19 21:36:10 +0800

    展示首页活动的web接口具体实现

--- src/main/java/com/github/oneone1995/mvolunteer/web/ActivityController.java
+++ src/main/java/com/github/oneone1995/mvolunteer/web/ActivityController.java
@@ -1,10 +1,19 @@
 package com.github.oneone1995.mvolunteer.web;
 
+import com.github.oneone1995.mvolunteer.config.result.ResultStatus;
+import com.github.oneone1995.mvolunteer.domain.HomeActivity;
+import com.github.oneone1995.mvolunteer.model.ResultModel;
+import com.github.oneone1995.mvolunteer.service.ActivityService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by wangl on 2017/2/18.
@@ -13,13 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/activity")
 public class ActivityController {
 
+    @Autowired
+    private ActivityService activityService;
+
     @GetMapping
     public ResponseEntity<?> getActivities(
             @RequestParam(value = "page") Integer page,
             @RequestParam(value = "rows", defaultValue = "10") Integer rows,
             @RequestParam(value = "coordLong") double coordLong,
             @RequestParam(value = "coordLat") double coordLat) {
-        //todo 返回活动信息
-        return null;
+        PageInfo<List<HomeActivity>> homeActivityPageInfo = activityService.getHomeActivityPageInfo(
+                page, rows, coordLong, coordLong);
+        if (homeActivityPageInfo == null) {
+            return new ResponseEntity<>(ResultModel.error(ResultStatus.ACTIVITY_NOT_FOUNT), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(ResultModel.ok(homeActivityPageInfo), HttpStatus.OK);
     }
 }
