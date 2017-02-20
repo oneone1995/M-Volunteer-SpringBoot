587217e413e19478f33a83550b0eb74b80321234
oneone1995 <wanglixian1995@live.com>
2017-02-20 23:33:44 +0800

    修复错误

--- src/main/java/com/github/oneone1995/mvolunteer/web/SearchController.java
+++ src/main/java/com/github/oneone1995/mvolunteer/web/SearchController.java
@@ -8,10 +8,7 @@ import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,20 +16,21 @@ import java.util.List;
  * Created by wangl on 2017/2/20.
  * 搜索有关controller
  */
-@RestController("/api/search")
+@RestController
+@RequestMapping("/api/search")
 public class SearchController {
 
     @Autowired
     private SearchService searchService;
 
 
-    @GetMapping("activity/{activityname}")
+    @GetMapping("activity")
     public ResponseEntity<?> getActivitySearchResult(
             @RequestParam(value = "page") Integer page,
             @RequestParam(value = "rows", defaultValue = "10") Integer rows,
             @RequestParam(value = "coordLong") double coordLong,
             @RequestParam(value = "coordLat") double coordLat,
-            @PathVariable String activityName
+            @RequestParam(value = "activityName") String activityName
     ) {
         PageInfo<List<HomeActivity>> searchActivityPageInfo = searchService.getHomeActivityPageInfo(
                 page, rows, coordLong, coordLat, activityName);
