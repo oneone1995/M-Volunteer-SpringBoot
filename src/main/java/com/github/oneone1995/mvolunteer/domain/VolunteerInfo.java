ef343c98c90db8f36604ce122250b27c8a8d6465
oneone1995 <wanglixian1995@live.com>
2017-02-21 19:24:56 +0800

    新增由mybatis-generator生成的VolunteerInfo类

---
+++ src/main/java/com/github/oneone1995/mvolunteer/domain/VolunteerInfo.java
@@ -0,0 +1,125 @@
+package com.github.oneone1995.mvolunteer.domain;
+
+import java.util.Date;
+
+public class VolunteerInfo {
+    private Integer id;
+
+    private String name;
+
+    private String idCard;
+
+    private Integer sex;
+
+    private Integer age;
+
+    private Date birthday;
+
+    private String politicalStatus;
+
+    private String address;
+
+    private String addressProvince;
+
+    private String addressCity;
+
+    private Double workingHours;
+
+    private String avatar;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPoliticalStatus() {
+        return politicalStatus;
+    }
+
+    public void setPoliticalStatus(String politicalStatus) {
+        this.politicalStatus = politicalStatus;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddressProvince() {
+        return addressProvince;
+    }
+
+    public void setAddressProvince(String addressProvince) {
+        this.addressProvince = addressProvince;
+    }
+
+    public String getAddressCity() {
+        return addressCity;
+    }
+
+    public void setAddressCity(String addressCity) {
+        this.addressCity = addressCity;
+    }
+
+    public Double getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(Double workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
