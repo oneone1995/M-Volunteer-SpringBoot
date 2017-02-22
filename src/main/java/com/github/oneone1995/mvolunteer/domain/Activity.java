bb42117dcca1f838c704708c8357b97581d3c040
oneone1995 <wanglixian1995@live.com>
2017-02-22 11:29:46 +0800

    新增活动和活动状态实体类

---
+++ src/main/java/com/github/oneone1995/mvolunteer/domain/Activity.java
@@ -0,0 +1,235 @@
+package com.github.oneone1995.mvolunteer.domain;
+
+import java.util.Date;
+
+public class Activity {
+    private Integer id;
+
+    private String addressProvince;
+
+    private String addressCity;
+
+    private String addressDistrict;
+
+    private String addressStreet;
+
+    private String address;
+
+    private Double coordLong;
+
+    private Double coordLat;
+
+    private String serviceType;
+
+    private String serviceObject;
+
+    private String recruitTime;
+
+    private Integer recruitPersonNumber;
+
+    private String name;
+
+    private String time;
+
+    private String principalName;
+
+    private String principalContact;
+
+    private Integer activityStatusId;
+
+    private Double workingHours;
+
+    private Date timestamp;
+
+    private String picture;
+
+    private Integer code;
+
+    private Integer organizationId;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddressProvince() {
+        return addressProvince;
+    }
+
+    public void setAddressProvince(String addressProvince) {
+        this.addressProvince = addressProvince;
+    }
+
+    public String getAddressCity() {
+        return addressCity;
+    }
+
+    public void setAddressCity(String addressCity) {
+        this.addressCity = addressCity;
+    }
+
+    public String getAddressDistrict() {
+        return addressDistrict;
+    }
+
+    public void setAddressDistrict(String addressDistrict) {
+        this.addressDistrict = addressDistrict;
+    }
+
+    public String getAddressStreet() {
+        return addressStreet;
+    }
+
+    public void setAddressStreet(String addressStreet) {
+        this.addressStreet = addressStreet;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getCoordLong() {
+        return coordLong;
+    }
+
+    public void setCoordLong(Double coordLong) {
+        this.coordLong = coordLong;
+    }
+
+    public Double getCoordLat() {
+        return coordLat;
+    }
+
+    public void setCoordLat(Double coordLat) {
+        this.coordLat = coordLat;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getServiceObject() {
+        return serviceObject;
+    }
+
+    public void setServiceObject(String serviceObject) {
+        this.serviceObject = serviceObject;
+    }
+
+    public String getRecruitTime() {
+        return recruitTime;
+    }
+
+    public void setRecruitTime(String recruitTime) {
+        this.recruitTime = recruitTime;
+    }
+
+    public Integer getRecruitPersonNumber() {
+        return recruitPersonNumber;
+    }
+
+    public void setRecruitPersonNumber(Integer recruitPersonNumber) {
+        this.recruitPersonNumber = recruitPersonNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPrincipalName() {
+        return principalName;
+    }
+
+    public void setPrincipalName(String principalName) {
+        this.principalName = principalName;
+    }
+
+    public String getPrincipalContact() {
+        return principalContact;
+    }
+
+    public void setPrincipalContact(String principalContact) {
+        this.principalContact = principalContact;
+    }
+
+    public Integer getActivityStatusId() {
+        return activityStatusId;
+    }
+
+    public void setActivityStatusId(Integer activityStatusId) {
+        this.activityStatusId = activityStatusId;
+    }
+
+    public Double getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(Double workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Integer organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/github/oneone1995/mvolunteer/domain/ActivityStatus.java
@@ -0,0 +1,23 @@
+package com.github.oneone1995.mvolunteer.domain;
+
+public class ActivityStatus {
+    private Integer id;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
