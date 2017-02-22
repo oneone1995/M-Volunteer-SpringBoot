68d5907fe112ed3db345d098f71f63b11de25bfd
oneone1995 <wanglixian1995@live.com>
2017-02-22 23:37:02 +0800

    完善根据当前登录用户查询其参加的活动列表的web接口

--- src/main/java/com/github/oneone1995/mvolunteer/web/VolunteerController.java
+++ src/main/java/com/github/oneone1995/mvolunteer/web/VolunteerController.java
@@ -2,10 +2,13 @@ package com.github.oneone1995.mvolunteer.web;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.github.oneone1995.mvolunteer.config.result.ResultStatus;
+import com.github.oneone1995.mvolunteer.domain.ActivityDetails;
 import com.github.oneone1995.mvolunteer.domain.CustomUserDetails;
 import com.github.oneone1995.mvolunteer.domain.VolunteerInfo;
 import com.github.oneone1995.mvolunteer.model.ResultModel;
+import com.github.oneone1995.mvolunteer.service.ActivityService;
 import com.github.oneone1995.mvolunteer.service.VolunteerService;
+import com.github.pagehelper.PageInfo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +16,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by wangl on 2017/2/22.
@@ -27,6 +32,9 @@ public class VolunteerController {
 
     @Resource
     private VolunteerService volunteerService;
+
+    @Resource
+    private ActivityService activityService;
 
     @GetMapping
     @PreAuthorize("hasRole('ROLE_VOL')")
@@ -41,11 +49,20 @@ public class VolunteerController {
         return new ResponseEntity<>(ResultModel.ok(volunteerInfo), HttpStatus.OK);
     }
 
-    @GetMapping("/volunteer")
+    @GetMapping("/activities")
     @PreAuthorize("hasRole('ROLE_VOL')")
-    public ResponseEntity<?> getActivitiesOfCurrentUser() {
+    public ResponseEntity<?> getActivitiesOfCurrentUser(
+            @RequestParam(value = "page") Integer page,
+            @RequestParam(value = "rows", defaultValue = "10") Integer rows
+    ) {
         CustomUserDetails currentUser = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Integer id = currentUser.getId();
-        return null;
+
+        PageInfo<ActivityDetails> activityDetailsPageInfo = activityService.getActivityPageInfoByVolunteerId(
+                page, rows, id);
+        if (activityDetailsPageInfo == null) {
+            return new ResponseEntity<>(ResultModel.error(ResultStatus.ACTIVITY_NOT_FOUNT), HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(ResultModel.ok(activityDetailsPageInfo), HttpStatus.OK);
     }
 }
