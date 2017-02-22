9128d05983c1b60a5fe9bb88c5da119a0f7dea61
oneone1995 <wanglixian1995@live.com>
2017-02-22 19:27:12 +0800

    完善获取志愿者个人详情的web接口

--- src/main/java/com/github/oneone1995/mvolunteer/web/VolunteerController.java
+++ src/main/java/com/github/oneone1995/mvolunteer/web/VolunteerController.java
@@ -1,11 +1,20 @@
 package com.github.oneone1995.mvolunteer.web;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.github.oneone1995.mvolunteer.config.result.ResultStatus;
 import com.github.oneone1995.mvolunteer.domain.CustomUserDetails;
+import com.github.oneone1995.mvolunteer.domain.VolunteerInfo;
+import com.github.oneone1995.mvolunteer.model.ResultModel;
+import com.github.oneone1995.mvolunteer.service.VolunteerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by wangl on 2017/2/22.
@@ -15,10 +24,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/volunteer")
 public class VolunteerController {
 
+    @Resource
+    private VolunteerService volunteerService;
+
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_VOL')")
     public ResponseEntity<?> getVolunteerById() {
         CustomUserDetails currentUser = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Integer id = currentUser.getId();
-        return null;
+        VolunteerInfo volunteerInfo = volunteerService.getVolunteerById(id);
+
+        if (volunteerInfo == null) {
+            return new ResponseEntity<>(ResultModel.error(ResultStatus.USER_NOT_FOUND), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(ResultModel.ok(volunteerInfo), HttpStatus.OK);
     }
 }
