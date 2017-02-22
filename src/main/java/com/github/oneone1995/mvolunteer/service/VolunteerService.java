349ae000d55b21108e820e0a4b4511b13aa9ac5a
oneone1995 <wanglixian1995@live.com>
2017-02-22 16:31:21 +0800

    新增根据id获取志愿者信息的业务接口

---
+++ src/main/java/com/github/oneone1995/mvolunteer/service/VolunteerService.java
@@ -0,0 +1,10 @@
+package com.github.oneone1995.mvolunteer.service;
+
+import com.github.oneone1995.mvolunteer.domain.VolunteerInfo;
+
+/**
+ * Created by wangl on 2017/2/22.
+ */
+public interface VolunteerService {
+    VolunteerInfo getVolunteerById(Integer id);
+}
