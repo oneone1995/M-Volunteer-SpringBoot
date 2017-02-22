78395753f3d0c6d7806451acc1ff20dbc8bceb64
oneone1995 <wanglixian1995@live.com>
2017-02-22 22:43:07 +0800

    设置Activity中为null的字段不参与json序列化

--- src/main/java/com/github/oneone1995/mvolunteer/domain/Activity.java
+++ src/main/java/com/github/oneone1995/mvolunteer/domain/Activity.java
@@ -1,7 +1,10 @@
 package com.github.oneone1995.mvolunteer.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Date;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Activity {
     private Integer id;
 
