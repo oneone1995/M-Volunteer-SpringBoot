3aea6ba86a122102a18a1a3f426137fd53efbd9f
oneone1995 <wanglixian1995@live.com>
2017-02-22 15:29:21 +0800

    添加根据id查询活动状态的业务接口

---
+++ src/main/java/com/github/oneone1995/mvolunteer/service/ActivityStatusService.java
@@ -0,0 +1,10 @@
+package com.github.oneone1995.mvolunteer.service;
+
+import com.github.oneone1995.mvolunteer.domain.ActivityStatus;
+
+/**
+ * Created by wangl on 2017/2/22.
+ */
+public interface ActivityStatusService {
+    ActivityStatus getActivityStatusById(Integer id);
+}
