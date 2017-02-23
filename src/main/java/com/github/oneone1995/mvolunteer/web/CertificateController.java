7c9b17aa14c0a12ac36e2f297dd902f1acc03a0e
oneone1995 <wanglixian1995@live.com>
2017-02-23 17:45:04 +0800

    完善申请证书的web接口

--- src/main/java/com/github/oneone1995/mvolunteer/web/CertificateController.java
+++ src/main/java/com/github/oneone1995/mvolunteer/web/CertificateController.java
@@ -1,9 +1,15 @@
 package com.github.oneone1995.mvolunteer.web;
 
+import com.github.oneone1995.mvolunteer.config.result.ResultStatus;
 import com.github.oneone1995.mvolunteer.domain.Certificate;
+import com.github.oneone1995.mvolunteer.model.ResultModel;
+import com.github.oneone1995.mvolunteer.service.CertificateService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * Created by wangl on 2017/2/23.
@@ -12,11 +18,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/certificate")
 public class CertificateController {
 
+    @Resource
+    private CertificateService certificateService;
+
     @PostMapping
     @PreAuthorize("hasRole('ROLE_VOL')")
     public ResponseEntity<?> postCertificate(
             @RequestBody Certificate certificate
             ) {
-        return null;
+        String result = certificateService.applyForCertificate(certificate);
+
+        if (result.equals("ALREADY_ERROR"))
+            return new ResponseEntity<>(ResultModel.error(ResultStatus.CERTIFICATE_IS_EXIST), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        if (result.equals("INSUFFICIENT_ERROR")) {
+            return new ResponseEntity<>(ResultModel.error(ResultStatus.WORKING_HOURS_NOT_ENOUGH), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(ResultModel.ok(result), HttpStatus.OK);
     }
 }
