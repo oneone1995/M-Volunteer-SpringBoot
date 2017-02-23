70971dc7402f45d3bfd4f7190f1c82ab415e1e12
oneone1995 <wanglixian1995@live.com>
2017-02-23 21:37:26 +0800

    新增根据用户id和活动id查询出关系表中符合条件的记录的id的mapper接口

--- src/main/java/com/github/oneone1995/mvolunteer/mapper/ActivityUserMapper.java
+++ src/main/java/com/github/oneone1995/mvolunteer/mapper/ActivityUserMapper.java
@@ -2,6 +2,7 @@ package com.github.oneone1995.mvolunteer.mapper;
 
 import com.github.oneone1995.mvolunteer.domain.ActivityUser;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -16,4 +17,15 @@ public interface ActivityUserMapper {
     List<ActivityUser> selectAll();
 
     int updateByPrimaryKey(ActivityUser record);
+
+    /**
+     * 根据用户id和活动id查询出关系表中符合条件的记录的id
+     * @param activityId    活动id
+     * @param userId    用户id
+     * @return  活动用户关系表的主键id
+     */
+    Integer selectPrimaryKeyByUserIdAndActivityId(
+            @Param("activityId") Integer activityId,
+            @Param("userId") Integer userId
+    );
 }
