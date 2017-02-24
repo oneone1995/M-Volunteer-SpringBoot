a877fc147149f5b58d6f9ce4d597d3044b006b54
oneone1995 <wanglixian1995@live.com>
2017-02-25 00:28:49 +0800

    新增根据用户id数组和活动工时更新用户工时的mapper接口和对应的sql

--- src/main/java/com/github/oneone1995/mvolunteer/mapper/VolunteerInfoMapper.java
+++ src/main/java/com/github/oneone1995/mvolunteer/mapper/VolunteerInfoMapper.java
@@ -2,6 +2,7 @@ package com.github.oneone1995.mvolunteer.mapper;
 
 import com.github.oneone1995.mvolunteer.domain.VolunteerInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -16,4 +17,15 @@ public interface VolunteerInfoMapper {
     List<VolunteerInfo> selectAll();
 
     int updateByPrimaryKey(VolunteerInfo record);
+
+
+    /**
+     * 根据用户id数组和活动工时更新用户工时
+     * @param userIds   用户id
+     * @param workingHours  工时
+     * @return  更新记录数
+     */
+    int updateWorkingHoursByIdAndWorkingHours(
+            @Param("userIds") List<Integer> userIds,
+            @Param("workingHours") double workingHours);
 }
