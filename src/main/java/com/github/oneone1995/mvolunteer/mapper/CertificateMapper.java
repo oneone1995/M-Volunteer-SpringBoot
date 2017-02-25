61196a9ef6dbf287eb21fcc2d1414ae9a5ec1f76
oneone1995 <wanglixian1995@live.com>
2017-02-25 13:20:51 +0800

    新增根据志愿者id更新证书状态的mapper接口以及对应sql

--- src/main/java/com/github/oneone1995/mvolunteer/mapper/CertificateMapper.java
+++ src/main/java/com/github/oneone1995/mvolunteer/mapper/CertificateMapper.java
@@ -2,6 +2,7 @@ package com.github.oneone1995.mvolunteer.mapper;
 
 import com.github.oneone1995.mvolunteer.domain.Certificate;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -16,4 +17,12 @@ public interface CertificateMapper {
     List<Certificate> selectAll();
 
     int updateByPrimaryKey(Certificate record);
+
+    /**
+     * 根据志愿者id更新证书状态
+     * @param volunteerId   志愿者id
+     * @return  更新记录数
+     */
+    Integer updateByVolunteerId(
+            @Param("volunteerId") Integer volunteerId);
 }
