1670a556092e127ee3bc1d46ab80742aa9c204b9
oneone1995 <wanglixian1995@live.com>
2017-02-25 12:49:44 +0800

    新增根据组织id查询该组织的志愿者的列表信息的业务代码接口并具体实现

--- src/main/java/com/github/oneone1995/mvolunteer/service/VolunteerService.java
+++ src/main/java/com/github/oneone1995/mvolunteer/service/VolunteerService.java
@@ -1,10 +1,15 @@
 package com.github.oneone1995.mvolunteer.service;
 
+import com.github.oneone1995.mvolunteer.domain.VolunteerDetails;
 import com.github.oneone1995.mvolunteer.domain.VolunteerInfo;
+import com.github.pagehelper.PageInfo;
 
 /**
  * Created by wangl on 2017/2/22.
  */
 public interface VolunteerService {
     VolunteerInfo getVolunteerById(Integer id);
+
+    PageInfo<VolunteerDetails> getVolunteerListPageInfoByOrganizationId(
+            Integer page, Integer rows, Integer id);
 }
--- src/main/java/com/github/oneone1995/mvolunteer/service/impl/VolunteerServiceImpl.java
+++ src/main/java/com/github/oneone1995/mvolunteer/service/impl/VolunteerServiceImpl.java
@@ -1,10 +1,15 @@
 package com.github.oneone1995.mvolunteer.service.impl;
 
+import com.github.oneone1995.mvolunteer.domain.VolunteerDetails;
 import com.github.oneone1995.mvolunteer.domain.VolunteerInfo;
 import com.github.oneone1995.mvolunteer.mapper.VolunteerInfoMapper;
 import com.github.oneone1995.mvolunteer.service.VolunteerService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by wangl on 2017/2/22.
@@ -22,5 +27,18 @@ public class VolunteerServiceImpl implements VolunteerService {
             return null;
         }
         return volunteerInfo;
+    }
+
+    @Override
+    public PageInfo<VolunteerDetails> getVolunteerListPageInfoByOrganizationId(
+            Integer page, Integer rows, Integer id) {
+
+        PageHelper.startPage(page, rows);
+        List<VolunteerDetails> volunteerDetailsList = volunteerInfoMapper.selectByOrganizationId(id);
+
+        if (volunteerDetailsList == null || volunteerDetailsList.isEmpty()) {
+            return null;
+        }
+        return new PageInfo<>(volunteerDetailsList);
     }
 }
