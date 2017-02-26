7b6ca18a22f669852beaa0e643beecbd7e933424
oneone1995 <wanglixian1995@live.com>
2017-02-26 21:34:28 +0800

    新增一个阿里悟空的常量配置类，配置了app_key、app_security和访问的api接口地址

---
+++ src/main/java/com/github/oneone1995/mvolunteer/config/aliim/IMConstantConfig.java
@@ -0,0 +1,16 @@
+package com.github.oneone1995.mvolunteer.config.aliim;
+
+/**
+ * Created by wangl on 2017/2/26.
+ * 阿里悟空的常量配置类
+ * 配置了app_key、app_security和访问的api接口地址
+ */
+public class IMConstantConfig {
+    public static final String APP_KEY = "23653358";
+    public static final String APP_SECURITY = "a087591f9fcec915ebbc9dc1fa490c0f";
+    public static final String API_URL = "http://gw.api.taobao.com/router/rest";
+
+    private IMConstantConfig() {
+
+    }
+}
